60c3eeba4dc83644f01dc3f41f440afaf8ae1963
cominiapp <cominiapp@gmail.com>
2025-11-22 16:26:04 -0700

    Update error_handler.dart

--- lib/core/lib/core/error_handler.dart
+++ lib/core/lib/core/error_handler.dart
@@ -1,1 +1,8 @@
-
+class ErrorHandler {
+  static void logError(Object error, [StackTrace? stackTrace]) {
+    print("❌ ERROR: $error");
+    if (stackTrace != null) {
+      print("STACKTRACE: $stackTrace");
+    }
+  }
+}
